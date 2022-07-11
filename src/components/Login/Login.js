4c34d7ae9ab34f1eaae4467a90b889cfc5aa75da
Sarthak Modhe <sarthakpravin08@gmail.com>
2022-07-11 21:15:51 +0530

    added media queries1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import { GoogleButton } from "react-google-button";
@@ -13,23 +13,23 @@ const Login = () => {
     setData({ ...data, [input.name]: input.value });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const url = "http://localhost:8080/api/auth";
-      const { data: res } = await axios.post(url, data);
-      localStorage.setItem("token", res.data);
-      window.location = "/";
-    } catch (error) {
-      if (
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status <= 500
-      ) {
-        setError(error.response.data.message);
-      }
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const url = "http://localhost:8080/api/auth";
+  //     const { data: res } = await axios.post(url, data);
+  //     localStorage.setItem("token", res.data);
+  //     window.location = "/";
+  //   } catch (error) {
+  //     if (
+  //       error.response &&
+  //       error.response.status >= 400 &&
+  //       error.response.status <= 500
+  //     ) {
+  //       setError(error.response.data.message);
+  //     }
+  //   }
+  // };
 
   const {googleSignIn} = UserAuth();
 
@@ -45,7 +45,7 @@ const Login = () => {
     <div className={styles.login_container}>
       <div className={styles.login_form_container}>
         <div className={styles.left}>
-          <form className={styles.form_container} onSubmit={handleSubmit}>
+          <form className={styles.form_container}>
             <h1>Login to Your Account</h1>
             <input
               type="email"
